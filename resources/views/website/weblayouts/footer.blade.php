88c9db1ec6a984fa3a7396235e560c9aab6fc842
koshtisarita <koshtisarita27@gmail.com>
2022-04-28 14:45:53 +0530

    Small changes

--- resources/views/website/weblayouts/footer.blade.php
+++ resources/views/website/weblayouts/footer.blade.php
@@ -237,7 +237,6 @@
 									<div class="size-203 flex-c-m respon6">
 										Size
 									</div>
-
 									<div class="size-204 respon6-next">
 										<div class="rs1-select2 bor8 bg0">
 										<span class="size_dropdown"></span>
